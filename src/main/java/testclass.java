d2fbfaadfead2c1a33f1c2e3295d9de08397277b
cnuchellas <cnuchella1@gmail.com>
2020-12-19 17:54:56 -0500

    Added Docker file in project and deleting in resources

--- src/main/java/testclass.java
+++ src/main/java/testclass.java
@@ -3,6 +3,11 @@ public class testclass {
 public static void main(String [] args)
 {
     System.out.printf ( "srinu" );
+    System.out.printf ( "pandi" );
+
+    test3 test3 = new test3 ();
+
+    test3.display ();
 }
 
 }
